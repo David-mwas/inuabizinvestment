fa008427a9645110e4b6b5a945a47b5c4ae4377b
David-mwas <dmwas704@gmail.com>
2024-02-27 20:23:48 +0300

    Project set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p className="text-red-500">hello</p>
+    </div>
+  );
+}
+
+export default App;
